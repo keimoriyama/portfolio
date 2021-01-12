b5f5eef990196c5659436995a6c624b2747ce8f6
kei moriyama <keischwiiz@gmail.com>
2021-01-12 20:28:32 +0900

    cssの追加

--- src/navibar.js
+++ src/navibar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './navibar.css'
 
 class NaviBar extends React.Component{
     render(){
         return(
             <div>
-                <Link to="/">Home</Link>
-                <Link to="/product">product</Link>
-                <Link to="/profile">profile</Link>
-                <Link to="/contact">contact</Link>
+                <Link to="/" className="bar">Home</Link>
+                <Link to="/product" className='bar'>product</Link>
+                <Link to="/profile" className='bar'>profile</Link>
+                <Link to="/contact" className='bar'>contact</Link>
             </div>
         )
     }
